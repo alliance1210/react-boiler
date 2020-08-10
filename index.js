2f0d6a741a6bcf211cd565cb8342c4c81df2d117
Pradei Sangkhrow <alliancetlr@gmail.com>
2020-08-10 16:53:01 +0530

    move private information to dev.js

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://Pradei:Password@321@cluster0.5gxxn.mongodb.net/<dbname>?retryWrites=true&w=majority',
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+
+const config = require('./config/key');
+
+const{ User } = require('./model/user');
+
+mongoose.connect(config.mongoURI,
 {useNewUrlParser: true, useUnifiedTopology: true  }).then(()=> console.log('Db connected'))
 .catch(err => console.error(err));
 
-app.get('/', (req, res)=>{
-    res.send('Hello world!!')
-});
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(cookieParser());
 
+app.get('/',(req,res)=>{
+    res.json({"hello~":"Hi~ fds"})
+})
 
+app.post('/api/users/register',(req,res)=>{
+    const user = new User(req.body)
+
+    user.save((err, userData)=> {
+        if(err) return res.json({success: false, err})
+   
+
+    return res.status(200).json({
+        success:true
+    })
+})
+})
 
 
 
